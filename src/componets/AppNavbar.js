c7fb1127ffc6789e1bc4084807a1b018073772c0
AlexVcom <doalvor@gmail.com>
2020-08-21 21:49:36 +0200

    fix navbar

--- src/componets/AppNavbar.js
+++ src/componets/AppNavbar.js
@@ -18,7 +18,6 @@ export default class AppNavbar extends Component {
   }
 
   render() {
-    // return musi zwracać środek 
     return <Navbar color="dark" dark expand="md">
         <NavbarBrand tag={Link} to="/home">Baby App</NavbarBrand>
         <NavbarToggler onClick={this.toggle} />
@@ -33,41 +32,7 @@ export default class AppNavbar extends Component {
     
           </Nav>
         </Collapse>
-      </Navbar>;
-
-
-
-
-
-    // <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    //   <a className="navbar-brand" href="/">Baby App</a>
-    //   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    //     <span className="navbar-toggler-icon"></span>
-    //   </button>
-    //   <div className="collapse navbar-collapse" id="navbarNav">
-    //     <ul className="navbar-nav">
-    //       <li className="nav-item active">
-    //         <a className="nav-link" href="/home">HOME <span className="sr-only">(current)</span></a>
-    //       </li>
-    //       <li className="nav-item">
-    //         <a className="nav-link" href="/contact">CONTACT</a>
-    //       </li>
-    //       <li className="nav-item">
-    //         <a className="nav-link" href="/about">ABOUT</a>
-    //       </li>
-    //       <li className="nav-item">
-    //         <a className="nav-link" href="/user">USER</a>
-    //       </li>
-    //       <li className="nav-item ml-auto">
-    //         <a className="nav-link" href="/login" >LOGIN</a>
-    //       </li>
-
-    //     </ul>
-    //   </div>
-
-    // </nav>
-  
-
+      </Navbar>;  
   }
 }
 
